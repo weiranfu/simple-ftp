4205a69ba987aecab0152ba4666899fd5b1d6319
Weiran Fu <weiranfu1@gmail.com>
2020-12-17 15:25:06 -0500

    first commit

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Client implements Runnable {
+
+    private DatagramSocket socket;
+    private InetAddress address;
+    private byte[] buf;
+
+    @Override
+    public void run() {
+
+    }
+}
