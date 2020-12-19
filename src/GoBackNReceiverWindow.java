90aaee86f88d7b2401855fb5c193bf70c2ad34f1
Weiran Fu <weiranfu1@gmail.com>
2020-12-19 17:12:30 -0500

    Update receiver window.

--- src/GoBackNReceiverWindow.java
+++ src/GoBackNReceiverWindow.java
@@ -1,38 +1,20 @@
-import java.net.DatagramPacket;
 
 public class GoBackNReceiverWindow {
-    private final int N;
-    private int left, right;
 
-    public GoBackNReceiverWindow(DatagramPacket[] packets, int windowSize) {
-        N = packets.length;
-        left = 0; right = N - 1;
-    }
+    private int next = 0;          // starting sequence number is 0 by default.
 
     /**
      * Determine whether window should receive this packet.
      * @param seq the index/sequence number of packet
      */
     public synchronized boolean shouldReceivedPacket(int seq) {
-        return left == seq;
+        return next == seq;
     }
 
     /**
      * Receives a packet.
      */
     public synchronized void receivePacket() {
-        if (left <= right) {
-            left++;
-        }
-        if (right < N - 1) {   // if right bound doesn't exceed N - 1
-            right++;
-        }
-    }
-
-    /**
-     * Determine whether all packets are received.
-     */
-    public synchronized boolean isFinished() {
-        return left > right;
+        next++;
     }
 }
