18147dacccd45c69ba17deaa0b56ff0e3f7980b1
Weiran Fu <weiranfu1@gmail.com>
2020-12-18 21:15:21 -0500

    update PseudoHeader API

--- src/PseudoHeader.java
+++ src/PseudoHeader.java
@@ -2,7 +2,6 @@ import java.net.InetAddress;
 
 public class PseudoHeader {
     private static final int MASK8 = 0xFF;
-    private final int HEADER_SIZE = 12;
     private final int SOURCE_IP_SIZE = 4;
     private final int DESTINATION_IP_SIZE = 4;
     private final int RESERVED_SIZE = 1;
@@ -10,6 +9,8 @@ public class PseudoHeader {
     private final int TOTAL_LENGTH_SIZE = 2;
     private final int UDP = 17;
     private final byte[] pseudoHeader;
+
+    public static final int HEADER_SIZE = 12;
 
     public PseudoHeader() {
         pseudoHeader = new byte[HEADER_SIZE];
@@ -20,10 +21,11 @@ public class PseudoHeader {
         System.arraycopy(address, 0, pseudoHeader, 0, SOURCE_IP_SIZE);
     }
 
-    public byte[] getSourceIP() {
-        byte[] address = new byte[SOURCE_IP_SIZE];
-        System.arraycopy(pseudoHeader, 0, address, 0, SOURCE_IP_SIZE);
-        return address;
+    public short getSourceIPCheckSum() {
+        short sum = 0;
+        sum += (pseudoHeader[0] << 8) | pseudoHeader[1];
+        sum += (pseudoHeader[2] << 8) | pseudoHeader[3];
+        return sum;
     }
 
     public void setDesIP(InetAddress serverAddress) {
@@ -31,13 +33,14 @@ public class PseudoHeader {
         System.arraycopy(address, 0, pseudoHeader, SOURCE_IP_SIZE, DESTINATION_IP_SIZE);
     }
 
-    public byte[] getDesIP() {
-        byte[] address = new byte[DESTINATION_IP_SIZE];
-        System.arraycopy(pseudoHeader, SOURCE_IP_SIZE, address, 0, DESTINATION_IP_SIZE);
-        return address;
+    public short getDesIPCheckSum() {
+        short sum = 0;
+        sum += (pseudoHeader[DESTINATION_IP_SIZE] << 8) | pseudoHeader[DESTINATION_IP_SIZE + 1];
+        sum += (pseudoHeader[DESTINATION_IP_SIZE + 2] << 8) | pseudoHeader[DESTINATION_IP_SIZE + 3];
+        return sum;
     }
 
-    public byte getReserved() {
+    public byte getReservedCheckSum() {
         return 0;
     }
 
@@ -58,10 +61,10 @@ public class PseudoHeader {
         pseudoHeader[index] = (byte) (length & MASK8);
     }
 
-    public byte[] getTotalLength() {
-        byte[] length = new byte[TOTAL_LENGTH_SIZE];
+    public short getTotalLength() {
         int index = SOURCE_IP_SIZE + DESTINATION_IP_SIZE + RESERVED_SIZE + PROTOCOL_SIZE;
-        System.arraycopy(pseudoHeader, index, length, 0, TOTAL_LENGTH_SIZE);
-        return length;
+        short sum = 0;
+        sum += (pseudoHeader[index] << 8) | pseudoHeader[index + 1];
+        return sum;
     }
 }
