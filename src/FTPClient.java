cd55b0b3f39c1d2dd41528a0fd9db5f3c477971c
Weiran Fu <weiranfu1@gmail.com>
2020-12-19 16:31:35 -0500

    Add rdt_send()

--- src/FTPClient.java
+++ src/FTPClient.java
@@ -2,13 +2,13 @@ import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 
 public class FTPClient implements Runnable {
 
-    private final short dataType = (short)21845;  // 0101010101010101
-    private final short ackType = (short)-21846;   // 1010101010101010
     private final int UDP_HEADER_SIZE = 8;
     private final int MASK16 = 0xFFFF;
+    private final int DELAY_TIME = 5000;
 
     private final String SERVER_HOSTNAME;
     private final int SERVER_PORT;
@@ -41,18 +41,40 @@ public class FTPClient implements Runnable {
             pseudoHeader.setSourceIP(localAddress);
             pseudoHeader.setDesIP(serverAddress);
             pseudoHeader.setProtocol("UDP");
+            List<Segment> segments = prepareSegments();
+            List<Timer> timers = new ArrayList<>();                // We don't need Thread-safe List, because Timer is thread-safe.
+            for (int i = 0; i < segments.size(); i++) timers.add(new Timer());
+            window = new GoBackNSenderWindow(segments.size(), MSS);
+            ACKReceiveThread receiveThread = new ACKReceiveThread(socket, window, MSS, timers);
+            new Thread(receiveThread).start();
+            // Begin to send UDP segments.
+            rdt_send(segments, timers, socket);
+            receiveThread.stop();
         } catch (SocketException e) {
             System.out.println("Cannot open a UDP socket: " + e.getMessage());
         } catch (UnknownHostException e) {
             System.out.println("Cannot find the address of localhost.");
+        } catch (IOException e) {
+            System.out.println("Cannot load file or send file with socket.");
+        } finally {
+            socket.close();
         }
     }
 
     /**
      * Reliable send data from file with UDP and Go-back-N.
      */
-    private void rdt_send() {
-        return;
+    private void rdt_send(List<Segment> segments, List<Timer> timers,DatagramSocket socket) throws IOException {
+        while (!window.isFinished()) {
+            int seq = window.sendPacketSeq();
+            if (seq != -1) {
+                byte[] data = segments.get(seq).toByteArray();
+                DatagramPacket packet = new DatagramPacket(data, data.length);
+                socket.send(packet);
+                timers.get(seq).schedule(new TimeoutTask(seq, window, timers), DELAY_TIME);
+                System.out.println("Sent packet, sequence number = " + seq);
+            }
+        }
     }
 
     private List<Segment> prepareSegments() throws IOException {
@@ -66,7 +88,7 @@ public class FTPClient implements Runnable {
             byte[] segmentByteArray = new byte[len + Segment.HEADER_SIZE];
             Segment segment = new Segment(segmentByteArray);
             segment.setSeqNum(seqNum++);
-            segment.setType(dataType);
+            segment.setType(Segment.dataType);
             segment.setData(data, offset, len);
             pseudoHeader.setTotalLength(len + Segment.HEADER_SIZE + UDP_HEADER_SIZE + PseudoHeader.HEADER_SIZE);
             segment.setCheckSum(calCheckSum(segment, pseudoHeader, data, offset, len));
