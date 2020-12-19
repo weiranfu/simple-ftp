dec519a1c7a128c5997dcf86a855e369cda728cd
Weiran Fu <weiranfu1@gmail.com>
2020-12-19 16:21:55 -0500

    Add timeout task for retransmit.

---
+++ src/TimeoutTask.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimeoutTask extends TimerTask {
+
+    private int seq;
+    private GoBackNSenderWindow window;
+    private List<Timer> timers;
+
+    public TimeoutTask(int seq, GoBackNSenderWindow window, List<Timer> timers) {
+        this.seq = seq;
+        this.window = window;
+        this.timers = timers;
+    }
+
+    @Override
+    public void run() {
+        int from = window.resetWindow(seq);
+        for (int i = seq + 1; i <= from; i++) {
+            timers.get(i).cancel();                 // cancel the timers of packets which need to be retransmitted.
+        }
+    }
+}
