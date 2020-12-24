87002428e2cd19ba2bd9f410e355599917c38f8e
Weiran Fu <weiranfu1@gmail.com>
2020-12-23 20:15:06 -0500

    Apply picocli to Server.

--- src/main/java/SimpleFTPServer.java
+++ src/main/java/SimpleFTPServer.java
@@ -1,7 +1,23 @@
-public class SimpleFTPServer {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "ftp-server", mixinStandardHelpOptions = true, version = "ftp-server 1.0",
+description = "A simple FTP server cli.")
+public class SimpleFTPServer implements Runnable {
+
+    @Option(names = {"-f", "--file"}, paramLabel = "<file path>", required = true, description = "The path of output file.")
+    private String filePath;
+    @Option(names = {"-p", "--possibility"}, paramLabel = "<loss possibility>", required = true, description = "The loss possibility of a packet.")
+    private double p;
+
+    @Override
+    public void run() {
+        FTPReceiver receiver = new FTPReceiver(filePath, p);
+        new Thread(receiver).start();
+    }
 
     public static void main(String[] args) {
-        FTPReceiver receiver = new FTPReceiver("./data/result.txt", 0.5);
-        new Thread(receiver).start();
+        new CommandLine(new SimpleFTPServer()).execute(args);
     }
 }
