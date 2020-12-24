5a8a985f83b1dfe0bc7abc6711e45b0bb59d6f05
Weiran Fu <weiranfu1@gmail.com>
2020-12-23 20:04:25 -0500

    Apply picocli to Client

--- src/main/java/SimpleFTPClient.java
+++ src/main/java/SimpleFTPClient.java
@@ -1,7 +1,28 @@
-public class SimpleFTPClient {
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+@CommandLine.Command(name = "ftp-client", mixinStandardHelpOptions = true, version = "ftp-client 1.0",
+description = "A simple FTP client cli.")
+public class SimpleFTPClient implements Runnable {
+
+    @Option(names = {"-H", "--hostname"}, paramLabel = "<hostname>", required = true, description = "The hostname of FTP server.")
+    private String hostname;
+    @Option(names = {"-p", "--port"}, paramLabel = "<port number>", required = true, description = "The port number of FTP server is listening on.")
+    private int port;
+    @Option(names = {"-f", "--file"}, paramLabel = "<file path>", required = true, description = "The path of file sent to FTP server.")
+    private String filePath;
+    @Option(names = {"-w", "--window"}, paramLabel = "<window size>", defaultValue = "8", description = "The sliding window size of Go-back-N algorithm.")
+    private int windowSize;
+    @Option(names = {"-m", "--mss"}, paramLabel = "<MSS>", defaultValue = "500", description = "The Maximum Segment Size(MSS) used for UDP segment.")
+    private int MSS;
+
+    @Override
+    public void run() {
+        FTPSender sender = new FTPSender(hostname, port, filePath, windowSize, MSS);
+        new Thread(sender).start();
+    }
 
     public static void main(String[] args) {
-        FTPSender sender = new FTPSender("localhost", 7735, "./data/rfc119.txt", 8, 200);
-        new Thread(sender).start();
+        new CommandLine(new SimpleFTPClient()).execute(args);
     }
 }
